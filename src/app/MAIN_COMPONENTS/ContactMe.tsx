846a20170cc7f58f8bbb394e80aeca27f45097da
MadRabbit001 <101290160+MadRabbit001@users.noreply.github.com>
2023-08-22 13:23:45 +0300

    static next website

--- src/app/MAIN_COMPONENTS/ContactMe.tsx
+++ src/app/MAIN_COMPONENTS/ContactMe.tsx
@@ -6,15 +6,15 @@ export default function ContactMe() {
         <div id={'contact'} className={'contact-section'}>
             <div className={'coop'}>
                 <div className={'coop-info'}>
-                    <p style={{fontSize:'1.5rem'}}>You have a new project ?</p>
-                    <p style={{fontSize:'1.2rem',width:'55%'}}>I would love to chat with you about your project and see how we can work together to create something truly amazing. Let&rsquo;s connect.</p>
+                    <p style={{fontSize:'150%'}}>You have a new project ?</p>
+                    <p style={{fontSize:'120%',width:'85%'}}>I would love to chat with you about your project and see how we can work together to create something truly amazing. Let&rsquo;s connect.</p>
                     <div className={'coop-button'}>
                         <p className={'skip'} style={{color:'#6243d3'}}>Contact Me</p>
-                        <NavigationIcon/>
+                        <NavigationIcon sx={{fontSize:'120%'}}/>
                     </div>
                 </div>
                 <div className={'coop-img'}>
-                    <img alt={'pic of me'}/>
+                    {/*<img src={'me3.png'} alt={'pic of me'}/>*/}
                 </div>
             </div>
             <div className={'actual-contact'}>
